932731e3ad460681c746e3918efd56b61f68579a
X4D0 <46711970+X4D0@users.noreply.github.com>
2020-11-04 08:42:07 +0700

    excel to lists

--- fuzzy.py
+++ fuzzy.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def readExcel():
     baca = pd.read_excel(r'Mahasiswa.xls')
-    return print(baca)
+    income = baca['Penghasilan'].tolist()
+    spending = baca['Pengeluaran'].tolist()
+    return income,spending
 
 readExcel()
